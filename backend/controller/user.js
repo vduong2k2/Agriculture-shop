caa5ab841fc35f7eb263ed23e76ebde14edc1218
vvduong2k2 <duongnguyen2002.bmt@gmail.com>
2023-04-22 00:35:47 +0700

    fix: controller user

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -5,35 +5,60 @@ const router = express.Router();
 const { upload } = require("../multer");
 const ErrorHandler = require("../utils/ErrorHandler");
 const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const sendMail = require("../utils/sendMail");
+const user = require("../model/user");
+
 router.post("/create-user", upload.single("file"), async (req, res, next) => {
-  const { name, email, password } = req.body;
-  const userEmail = await User.findOne({ email });
+  try {
+    const { name, email, password } = req.body;
+    const userEmail = await User.findOne({ email });
 
-  if (userEmail) {
+    if (userEmail) {
+      const filename = req.file.filename;
+      const filePath = `uploads/${filename}`;
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).json({ message: "Error deleting file" });
+        }
+      });
+      return next(new ErrorHandler("User already exists", 400));
+    }
+
     const filename = req.file.filename;
-    const filePath = `uploads/${filename}`;
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.log(err);
-        res.status(500).json({ message: "Error deleting file" });
-      }
-    });
-    return next(new ErrorHandler("User already exists", 400));
+    const fileUrl = path.join(filename);
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      avatar: fileUrl,
+    };
+    const activationToken = createActivationToken(user);
+
+    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+
+    try {
+      await sendMail({
+        email: user.email,
+        subject: "Activate your account",
+        message: `Hello ${user.name}, please click on the link to activate your account: ${activationUrl}`,
+      });
+      //   res.status(201).json({
+      //     success: true,
+      //     message: `please check your email:- ${user.email} to activate your account!`,
+      //   });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 400));
   }
-
-  const filename = req.file.filename;
-  const fileUrl = path.join(filename);
-  const user = {
-    name: name,
-    email: email,
-    password: password,
-    avatar: fileUrl,
-  };
-  const newUser = await User.create(user);
-  res.status(201).json({
-    success: true,
-    newUser,
-  });
 });
-
+// create activation token
+const createActivationToken = (user) => {
+  return jwt.sign(user, process.env.ACTIVATION_SECRET, {
+    expiresIn: "5m",
+  });
+};
 module.exports = router;
--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
-import { Form, Link } from "react-router-dom";
+import { Form, Link, useNavigate } from "react-router-dom";
 import { RxAvatar } from "react-icons/rx";
 import axios from "axios";
 import { server } from "../../server";
@@ -12,6 +12,7 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
   const [avatar, setAvatar] = useState(null);
+  const navigate = useNavigate();
 
   const handleFileInputChange = (e) => {
     const file = e.target.files[0];
@@ -31,7 +32,9 @@ const Signup = () => {
     axios
       .post(`${server}/user/create-user`, newForm, config)
       .then((res) => {
-        console.log(res);
+        if (res.data.success === true) {
+          navigate("/");
+        }
         // toast.success(res.data.message);
         // setName("");
         // setEmail("");
