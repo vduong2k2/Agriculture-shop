31c5fb5e3bd30ec7dad21c39ca04b3b1f974a031
Nguyễn Quốc Kha <Quockha081122@gmail.com>
2023-04-24 21:01:05 +0700

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { LoginPage, SignupPage, ActivationPage } from "./Routes.js";
-import { toast, ToastContainer } from "react-toastify";
+import { LoginPage, SignupPage, ActivationPage, HomePage } from "./Routes.js";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Store from "./redux/store";
 import { loadUser } from "./redux/actions/user";
@@ -14,6 +14,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
         <Route
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,4 +1,6 @@
 import LoginPage from "../src/pages/Login.jsx";
 import SignupPage from "../src/pages/SignupPage.jsx";
 import ActivationPage from "../src/pages/ActivationPage.jsx";
-export { LoginPage, SignupPage, ActivationPage };
+import HomePage from "../src/pages/HomePage";
+
+export { LoginPage, SignupPage, ActivationPage, HomePage };
