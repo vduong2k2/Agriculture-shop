cec41ab56d44af2ddcacef1d9a28e6eb3feb4c74
vvduong2k2 <duongnguyen2002.bmt@gmail.com>
2023-04-30 17:48:53 +0700

    updata: cart and wishlist

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -14,6 +14,8 @@ import Navbar from "./Navbar";
 import { CgProfile } from "react-icons/cg";
 import { useSelector } from "react-redux";
 import { backend_url } from "../../server";
+import Cart from "../cart/Cart.jsx";
+import Wishlist from "../Wishlist/Wishlist.jsx";
 const Header = ({ activeHeading }) => {
   const { isAuthenticated, user } = useSelector((state) => state.user);
   // const { isSeller } = useSelector((state) => state.seller);
@@ -25,8 +27,8 @@ const Header = ({ activeHeading }) => {
   const [active, setActive] = useState(false);
   const [dropDown, setDropDown] = useState(false);
 
-  // const [openCart, setOpenCart] = useState(false);
-  // const [openWishlist, setOpenWishlist] = useState(false);
+  const [openCart, setOpenCart] = useState(false);
+  const [openWishlist, setOpenWishlist] = useState(false);
   // const [open, setOpen] = useState(false);
   const handleSearchChange = (e) => {
     const term = e.target.value;
@@ -102,94 +104,99 @@ const Header = ({ activeHeading }) => {
             </Link>
           </div>
         </div>
-
+      </div>
+      <div
+        className={`${
+          active === true ? "shadow-sm fixed top-0 left-0 z-10" : null
+        } transition hidden 800px:flex items-center justify-between w-full bg-[#16a11d] h-[70px]`}
+      >
         <div
-          className={`${
-            active === true ? "shadow-sm fixed top-0 left-0 z-10" : null
-          } transition hidden 800px:flex items-center justify-between w-full bg-[#16a11d] h-[70px]`}
+          className={`${styles.section} relative ${styles.noramlFlex} justify-between`}
         >
-          <div
-            className={`${styles.section} relative ${styles.noramlFlex} justify-between`}
-          >
-            <div>
-              <div
-                className={
-                  "relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block"
-                }
+          <div>
+            <div
+              className={
+                "relative h-[60px] mt-[10px] w-[270px] hidden 1000px:block"
+              }
+            >
+              <BiMenuAltLeft
+                size={30}
+                className="absolute top-3 left-2"
+                //onClick={() => setOpen(true)}
+              />
+              <button
+                className={`h-[100%] w-full flex justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md`}
               >
-                <BiMenuAltLeft
-                  size={30}
-                  className="absolute top-3 left-2"
-                  //onClick={() => setOpen(true)}
+                All Categories
+              </button>
+              <IoIosArrowDown
+                size={20}
+                className="absolute right-2 top-4 cursor-pointer"
+                onClick={() => setDropDown(!dropDown)}
+              />
+              {dropDown ? (
+                <DropDown
+                  categoriesData={categoriesData}
+                  setDropDown={setDropDown}
                 />
-                <button
-                  className={`h-[100%] w-full flex justify-between items-center pl-10 bg-white font-sans text-lg font-[500] select-none rounded-t-md`}
-                >
-                  All Categories
-                </button>
-                <IoIosArrowDown
-                  size={20}
-                  className="absolute right-2 top-4 cursor-pointer"
-                  onClick={() => setDropDown(!dropDown)}
-                />
-                {dropDown ? (
-                  <DropDown
-                    categoriesData={categoriesData}
-                    setDropDown={setDropDown}
-                  />
-                ) : null}
-              </div>
+              ) : null}
             </div>
+          </div>
 
-            {/* navitems */}
+          {/* navitems */}
+          <div className={`${styles.noramlFlex}`}>
+            <Navbar active={activeHeading} />
+          </div>
+
+          <div className="flex">
             <div className={`${styles.noramlFlex}`}>
-              <Navbar active={activeHeading} />
-            </div>
-
-            <div className="flex">
-              <div className={`${styles.noramlFlex}`}>
-                <div
-                  className="relative cursor-pointer mr-[15px]"
-                  // onClick={() => setOpenWishlist(true)}
-                >
-                  <AiOutlineHeart size={30} color="rgb(255 255 255 / 83%)" />
-                  <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
-                    0
-                  </span>
-                </div>
-              </div>
-              <div className={`${styles.noramlFlex}`}>
-                <div
-                  className="relative cursor-pointer mr-[15px]"
-                  // onClick={() => setOpenWishlist(true)}
-                >
-                  <AiOutlineShoppingCart
-                    size={30}
-                    color="rgb(255 255 255 / 83%)"
-                  />
-                  <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
-                    0
-                  </span>
-                </div>
-              </div>
-              <div className={`${styles.noramlFlex}`}>
-                <div className="relative cursor-pointer mr-[15px]">
-                  {isAuthenticated ? (
-                    <Link to="/profile">
-                      <img
-                        src={`${backend_url}${user.avatar}`}
-                        className="w-[35px] h-[35px] rounded-full"
-                        alt=""
-                      />
-                    </Link>
-                  ) : (
-                    <Link to="/login">
-                      <CgProfile size={30} color="rgb(255 255 255 / 83%)" />
-                    </Link>
-                  )}
-                </div>
+              <div
+                className="relative cursor-pointer mr-[15px]"
+                onClick={() => setOpenWishlist(true)}
+              >
+                <AiOutlineHeart size={30} color="rgb(255 255 255 / 83%)" />
+                <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
+                  0
+                </span>
               </div>
             </div>
+            <div className={`${styles.noramlFlex}`}>
+              <div
+                className="relative cursor-pointer mr-[15px]"
+                onClick={() => setOpenCart(true)}
+              >
+                <AiOutlineShoppingCart
+                  size={30}
+                  color="rgb(255 255 255 / 83%)"
+                />
+                <span className="absolute right-0 top-0 rounded-full bg-[#3bc177] w-4 h-4 top right p-0 m-0 text-white font-mono text-[12px] leading-tight text-center">
+                  0
+                </span>
+              </div>
+            </div>
+            <div className={`${styles.noramlFlex}`}>
+              <div className="relative cursor-pointer mr-[15px]">
+                {isAuthenticated ? (
+                  <Link to="/profile">
+                    <img
+                      src={`${backend_url}${user.avatar}`}
+                      className="w-[35px] h-[35px] rounded-full"
+                      alt=""
+                    />
+                  </Link>
+                ) : (
+                  <Link to="/login">
+                    <CgProfile size={30} color="rgb(255 255 255 / 83%)" />
+                  </Link>
+                )}
+              </div>
+            </div>
+            {/* cart popup */}
+            {openCart ? <Cart setOpenCart={setOpenCart} /> : null}
+            {/* wishlist popup */}
+            {openWishlist ? (
+              <Wishlist setOpenWishlist={setOpenWishlist} />
+            ) : null}
           </div>
         </div>
       </div>
