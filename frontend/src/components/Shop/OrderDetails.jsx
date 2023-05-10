bf26f287483a08dbd1a676d47559edbcf2ff29c4
Nguyễn Quốc Kha <Quockha081122@gmail.com>
2023-05-10 22:03:58 +0700

    update

--- frontend/src/components/Shop/OrderDetails.jsx
+++ frontend/src/components/Shop/OrderDetails.jsx
@@ -100,7 +100,7 @@ const OrderDetails = () => {
             <div className="w-full">
               <h5 className="pl-3 text-[20px]">{item.name}</h5>
               <h5 className="pl-3 text-[20px] text-[#00000091]">
-                US${item.discountPrice} x {item.qty}
+                {item.discountPrice} VND x {item.qty}
               </h5>
             </div>
           </div>
