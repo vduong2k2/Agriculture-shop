90a9d5f8f9d562fb6f179baf723ae75007028018
Nguyễn Quốc Kha <Quockha081122@gmail.com>
2023-05-11 12:01:56 +0700

    update

--- frontend/src/components/Payment/Payment.jsx
+++ frontend/src/components/Payment/Payment.jsx
@@ -36,7 +36,7 @@ const Payment = () => {
           {
             description: "Sunflower",
             amount: {
-              currency_code: "USD",
+              currency_code: "VND",
               value: orderData?.totalPrice,
             },
           },
